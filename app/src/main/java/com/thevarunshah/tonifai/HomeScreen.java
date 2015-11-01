f7354118b7912f9028501f3d00d14402e9b5adac
Varun Shah <thevarunshah@gmail.com>
2015-10-31 23:21:39 -0400

    added contacts parsing code

--- app/src/main/java/com/thevarunshah/tonifai/HomeScreen.java
+++ app/src/main/java/com/thevarunshah/tonifai/HomeScreen.java
@@ -1,15 +1,93 @@
 package com.thevarunshah.tonifai;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class HomeScreen extends AppCompatActivity {
+
+    ListView lv;
+    ArrayAdapter<String> aa;
+    ArrayList<String> contacts = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
 
+        lv = (ListView) findViewById(R.id.contactsInfo);
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, contacts);
+        lv.setAdapter(aa);
 
+        Button fetch = (Button) findViewById(R.id.fetchContacts);
+        fetch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                readContacts();
+                aa.notifyDataSetChanged();
+            }
+        });
+    }
+
+    public void readContacts() {
+        StringBuffer sb = new StringBuffer();
+        //sb.append("......Contact Details.....");
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        String phone = null;
+        String emailContact = null;
+        String emailType = null;
+        String image_uri = "";
+        Bitmap bitmap = null;
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                image_uri = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_URI));
+                if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                    //System.out.println("name : " + name + ", ID : " + id);
+                    sb.append("\nContact Name: " + name);
+
+                    Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        phone = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        sb.append("\nPhone number: " + phone);
+                        //System.out.println("phone" + phone);
+                        break;
+                    }
+                    pCur.close();
+
+                    if (image_uri != null) {
+                        //System.out.println(Uri.parse(image_uri));
+                        try {
+                            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.parse(image_uri));
+                            sb.append("\nImage in Bitmap: " + bitmap);
+                            //System.out.println(bitmap);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    contacts.add(sb.toString());
+                    sb = new StringBuffer();
+                    //sb.append("\n........................................");
+                }
+            }
+        }
     }
 }
